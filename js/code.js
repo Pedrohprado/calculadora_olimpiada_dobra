0ca426a3302f57bef1772b3d78f964d06f5cea0f
Pedro Henrique Prado <pedro.prado@ptractor.com.br>
2022-03-31 07:45:06 -0300

    adicionado function para pegar os valores dos inputs

--- js/code.js
+++ js/code.js
@@ -1,21 +1,19 @@
-const enviar = document.querySelector(`#btn`) // "pegando" o input submit e colocando dentro de uma const
+const enviar = document.querySelector(`#btn`)
 
-enviar.addEventListener(`click`, function(e){   // criando um evento de click para o input submit
-    
-    e.preventDefault() // interromper o envio do form
+enviar.addEventListener(`click`, function(e){
+    e.preventDefault()
 
-    let bloco1 = document.querySelector(`#bloco1`).value 
-    let bloco2 = document.querySelector(`#bloco2`).value      // selecionando o ID.value dele e colocando esse valor dentro de uma let
-    let bloco3 = document.querySelector(`#bloco3`).value
-    let bloco4 = document.querySelector(`#bloco4`).value
+    const pegaValor = function(valor){
+        resultado = document.querySelector(valor).value
+        return resultado
+    }
 
+    let conta1 = pegaValor(`#bloco1`) * 0.750
+    let conta2 = pegaValor(`#bloco2`) * 1.071
+    let conta3 = pegaValor(`#bloco3`) * 1.077
+    let conta4 = pegaValor(`#bloco4`) * 2
 
-    let resultado1 = bloco1 * 0.750
-    let resultado2 = bloco2 * 1.071  // fazendo o calculo de cada questão 
-    let resultado3 = bloco3 * 1.077
-    let resultado4 = bloco4 * 2.000
+    const resultadoFinal = conta1 + conta2 + conta3 + conta4
 
-    const resultadoFinal = resultado1 + resultado2 + resultado3 + resultado4  // somando esses valores
-
-    document.querySelector(`.resultado`).innerHTML = `resultado final é: ${resultadoFinal} pontos` //  colocando o valor dentro do span com class .resultado
+    document.querySelector(`.resultado`).innerHTML = `resultado final é: ${resultadoFinal} pontos`
 })
